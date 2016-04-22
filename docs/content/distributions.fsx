2165a52c19624d72b1944dfd8f8e803c555c9844
Vassily Lyutsarev <vassilyl@microsoft.com>
2016-04-22 22:22:02 +0100

    continue distributions documentation

--- docs/content/distributions.fsx
+++ docs/content/distributions.fsx
@@ -2,98 +2,118 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin/Angara.Statistics"
+#r "../../packages/docs/Angara.Serialization/lib/net452/Angara.Serialization.dll"
+#r "../../packages/docs/Angara.Serialization.Json/lib/net452/Angara.Serialization.Json.dll"
+#r "../../packages/docs/Angara.Reinstate/lib/net452/Angara.Reinstate.dll"
+#r "../../packages/docs/Angara.Html/lib/net452/Angara.Html.dll"
+#r "../../packages/docs/Angara.Chart/lib/net452/Angara.Chart.dll"
 #r "Angara.Statistics.dll"
-(** We will use the following code to present a PDF and a histogram of distributions supported by the package. *)
+(**
+
+We will use the following code to present a PDF and a histogram of distributions supported by the package.
+This compares output of `draw` and `log_pdf` functions for the same distribution. 
+
+*)
 open Angara.Statistics
+open Angara.Charting
 
 let chart d xmin xmax =
-    let n, k, mt = 128, 1024, MT19937()
+    let n, k, mt = 128, 131072, MT19937()
     let h = Seq.init k (fun _ -> draw mt d) |> histogram_ n xmin xmax
     let xh = [|for i in 0..2*n+1 ->
-                xmin + float ((i+1)/2) * (xmax - xmin) / float n|]
+                xmin + float (i/2) * (xmax - xmin) / float n|]
     let yh = [|for i in 0..2*n+1 ->
-                if i=0 || i=2*n+1 then 0. else float(h.[(i-1)/2]) / float k |]
+                if i=0 || i=2*n+1 then 0. else float n / (xmax - xmin) * float(h.[(i-1)/2]) / float k |]
     let xpdf = [|for i in 0..n -> xmin + float i * (xmax - xmin) / float n|]
-    let ypdf = Array.map xpdf (fun x -> exp(log_pdf d x))
-
+    let ypdf = Array.map (fun x -> exp(log_pdf d x)) xpdf
+    Chart.ofList [Plot.line(xpdf, ypdf, thickness=7., stroke="lightgray")
+                  Plot.line(xh, yh)]
 
 (**
 
 ### Uniform distribution
 
-A `Distribution` is a value which allows to `draw` a pseudo-random number or compute a probability density function `log_pdf`.
-The `Distribution` discriminated union supports most common probability distributions like `Uniform` or `Normal`. More details are in a separate document page.
-
-For example, here is how you define normal distribution once you've referenced Angara.Statistics.dll:
+`Uniform(lower_bound, upper_bound)`, e.g.
 *)
-open Angara.Statistics
 
-let distribution = Normal(37.0, 9.0)
+let chart_uniform =
+    let d = Uniform(-1.,2.)
+    chart d -1.5 2.5
+
+(*** include-value:chart_uniform ***)
 (**
 
-### Random number generator
+### Log-uniform distribution
 
-To draw random numbers we use [Mersenne Twister](http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html),
-one of the most commonly used random number generator.
+`Uniform(lower_bound, upper_bound)`. A uniform distribution of `log(x)` is a non-uniform for `x`.
+Both bounds must be greater than zero. 
+
 *)
-let generator = MT19937()
-let random_values =
-    generator.uniform_uint32(),  // low-level output
-    generator.uniform_float64(), // a floating point number from [0,1)
-    generator.normal()           // standard normal distribution with mean=0 and stdev=1
-(*** include-value:random_values ***)
-(**
-To reproduce the generator state use either exact seed or copy-constructor:
-*)
-let seed = generator.get_seed()
-let g' = MT19937(seed)
-let g'' = MT19937(generator)
-// the three instances should now produce
-// exactly the same sequence of pseudo-random numbers
-let test_3 = generator.normal(), g'.normal(), g''.normal()
-(*** include-value:test_3 ***)
+
+let chart_loguniform =
+    let d = LogUniform(0.5,1.5)
+    chart d 0. 2.
+
+(*** include-value:chart_loguniform ***)
 (**
 
-To draw a number from a non-standard distribution use `draw : MT19337 -> Distribution -> float`.
-For example, here we generate an array of 1000 random numbers from the above distribution.
+### Normal distribution
+
+`Normal(mean, standard_deviation)`. This often used for real random values which exact distributions are not known. 
+
 *)
-let samples = [| for _ in 1..1000 -> draw generator distribution |]
+
+let chart_normal =
+    let d = Normal(37., 9.)
+    chart d 10. 64.
+
+(*** include-value:chart_normal ***)
 (**
 
-### Probability density function
+### Log-normal distribution
 
-One single function `log_pdf : Distribution -> float -> float` computes logarithm of normalized probability density
-of any distribution at any valid point.
+`LogNormal(mean, standard_deviation_log)`. A normal distribution of `log(x)`. The second parameter is a standard deviation of log(x), not a standard deviation of x. 
 
-If you do not have a Distribution but have a sample from the distribution used kernel density estimator
-`kde : int -> float seq -> (float[] * float[])`. The estimator needs target number of points at which the density is to be evaluated.
-If this number is not a power of two he number of points will be the next larger power of two. The estimator returns two arrays of `x` and `y` values:
-
-The following sample code compares non-parametric density of the sample with the exact density from `log_pdf` function.
 *)
-(*** define-output:sample ***)
-// estimate density curve in 16 points
-let sampling_density_x, sampling_density_y = kde 16 samples
-// compute exact probability density function
-let analytic_density_y = [|for x in sampling_density_x -> exp(log_pdf distribution x)|]
-// print the resulting table
-printfn ".   x   est.density exact.dnsty"
-Array.zip3 sampling_density_x sampling_density_y analytic_density_y
-|> Array.iter(fun (x, y1, y2) -> printfn "%6.1f %11.5f %11.5f" x y1 y2)
 
-(** The output of the above script:*)
-(*** include-output:sample ***)
+let chart_lognormal =
+    let d = LogNormal(37., 0.3)
+    chart d 10. 90.
 
+(*** include-value:chart_lognormal ***)
 (**
 
-### Sample statistics
+### Exponential distribution
 
-Use `summary` and `qsummary` functions to quickly compute mean, standard deviation, 95% and 68% quantiles of a sample:
+`Exponential(mean)`.
+It describes the time between events in a process
+in which events occur continuously and independently at a constant average rate.
+The only parameter must be greater than zero.
+
 *)
-(*** define-output:summary ***)
-printfn "%A" (summary samples)
-(*** include-output:summary ***)
-(*** define-output:qsummary ***)
-printfn "%A" (qsummary samples)
-(*** include-output:qsummary ***)
-(** The `lb95` in the `qsummary` record stands for "lower bound of 95% interval" with is actually a 2.5% quantile. Similarly `ub95` stands for 97.5% quantile. *)
+
+let chart_exponential =
+    let d = Exponential(1.)
+    chart d 0. 5.
+
+(*** include-value:chart_exponential ***)
+(**
+
+### Gamma distribution
+
+`Gamma(alpha, beta)`. This often used for real random values which exact distributions are not known. 
+
+*)
+
+let chart_gamma =
+    let d = Gamma(3.0, 0.5)
+    chart d 0. 20.
+
+(*** include-value:chart_gamma ***)
+(**
+
+### Gamma distribution
+
+`Exponential(mean)`. This often used for real random values which exact distributions are not known. 
+
+*)
